0a117732195f3e8503f4b0a00f2a3131cf730ef5
KEVYN DE ANDRADE LEITE <24122559@dsn127m1241955.cfp127aluno.local>
2025-03-06 12:23:19 -0300

    Último commit de 6 de março - Project_BMI

---
+++ app/src/main/java/br/senai/sp/jandira/bmi/screens/UserDataScreen.kt
@@ -0,0 +1,16 @@
+package br.senai.sp.jandira.bmi.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun UserDataScreen(modifier: Modifier = Modifier) {
+
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun UserDataScreenPreview() {
+    UserDataScreen()
+}
